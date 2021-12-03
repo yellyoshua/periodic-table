89e07fbdb7d24e683fe58a52f4ae4157e609e22e
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-03 12:15:29 -0500

    adding table periodic api service

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Container, TextHeader, TextMini, Grid } from "../components"
-import { Element } from "../interfaces"
-import ListElements from "../ui/ListElements"
-import Switch from "react-switch"
-
-
+import { Container, TextHeader, TextMini, Grid } from "../components";
+import { Element } from "../interfaces";
+import ListElements from "../ui/ListElements";
+import Switch from "react-switch";
 
 export const HomePage = () => {
   const [elements, setElements] = React.useState<Element[]>([]);
@@ -13,36 +11,40 @@ export const HomePage = () => {
   React.useEffect(() => {
     setElements([]);
     if (isActualTable) {
-      fetch("/elements_new.json")
-        .then(r => r.json())
-        .then(res => setElements(res))
-        .catch(() => setElements([]))
+      fetch(
+        "https://periodic-table.yellyoshua.workers.dev/api/new-periodic-table"
+      )
+        .then((r) => r.json())
+        .then((res) => setElements(res))
+        .catch(() => setElements([]));
     } else {
-      fetch("/elements.json")
-        .then(r => r.json())
-        .then(res => setElements(res))
-        .catch(() => setElements([]))
+      fetch("https://periodic-table.yellyoshua.workers.dev/api/periodic-table")
+        .then((r) => r.json())
+        .then((res) => setElements(res))
+        .catch(() => setElements([]));
     }
-  }, [isActualTable])
+  }, [isActualTable]);
 
-  return <Container>
-    <TextHeader>Lista de elementos de la tabla peri&oacute;dica</TextHeader>
+  return (
     <Container>
-      <Grid>
-        <Container>
-          <TextMini style={{fontWeight: "bold"}}>Tabla Antigua</TextMini>
-        </Container>
-        <Switch
-          checked={isActualTable}
-          uncheckedIcon={false}
-          checkedIcon={false}
-          onChange={setIsActualTable}
-        />
-        <Container>
-          <TextMini style={{fontWeight: "bold"}}>Tabla Nueva</TextMini>
-        </Container>
-      </Grid>
+      <TextHeader>Lista de elementos de la tabla peri&oacute;dica</TextHeader>
+      <Container>
+        <Grid>
+          <Container>
+            <TextMini style={{ fontWeight: "bold" }}>Tabla Antigua</TextMini>
+          </Container>
+          <Switch
+            checked={isActualTable}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            onChange={setIsActualTable}
+          />
+          <Container>
+            <TextMini style={{ fontWeight: "bold" }}>Tabla Nueva</TextMini>
+          </Container>
+        </Grid>
+      </Container>
+      <ListElements elements={elements} />
     </Container>
-    <ListElements elements={elements} />
-  </Container>
-}
+  );
+};
